5d4ee811a01aa2224fd6fba60cd3de4d70073cf7
Wildan <wildanabroriaziz@students.amikom.ac.id>
2023-03-16 14:48:29 +0700

    melengkapi fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,14 @@ class calculator
 {
 	static void main(string{}args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.Writelineriteline("hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.Writelineriteline("hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Consoleonsole.Writelineriteline("\nTekan sembarang Key untuk keluar");
+		Console.Readkey();
 	}
 	static int Penambahan(int a, int b)
 	{
